53f5cde3fbccd6c020b3a375561bb067d3cbc6da
Manasses Ndombele <manassesndombelefreelancer@gmail.com>
2025-03-26 06:58:42 +0100

    added a simple page to show really if backend is running

---
+++ backend/public/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>EDEN Hotels - Backend</title>
+</head>
+
+<body style="display: flex; min-height: 100vh; justify-content: center; align-items: center;">
+    <h1 style="text-align: center; text-transform: uppercase;">O Backend de EDEN Hotels está rodando com sucesso! 🫡
+    </h1>
+</body>
+
+</html>
