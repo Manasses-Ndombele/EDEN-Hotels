ad74a0c7f792f0ab8b1235a15c6d496be1e9a55a
Manasses Ndombele <manassesndombelefreelancer@gmail.com>
2025-03-29 12:15:30 +0100

    updated deploy configurations

--- backend/config/index.php
+++ backend/config/index.php
@@ -2,13 +2,24 @@
     require __DIR__ . "/../vendor/autoload.php";
     use Symfony\Component\Dotenv\Dotenv;
 
-    $dotenv = new Dotenv();
-    $dotenv->load(__DIR__ . '/../.env');
-    define("SECRET_KEY", $_ENV['SECRET_KEY']);
-    define("BASE_URL", $_ENV["BASE_URL"]);
-    define("DB_HOST", $_ENV["DB_HOST"]);
-    define("DB_USER", $_ENV["DB_USER"]);
-    define("DB_PASSWORD", $_ENV["DB_PASSWORD"]);
-    define("DB_NAME", $_ENV["DB_NAME"]);
-    define("DB_PORT", $_ENV["DB_PORT"]);
+    define("DEBUG_MODE", true);
+    if (DEBUG_MODE) {
+        $dotenv = new Dotenv();
+        $dotenv->load(__DIR__ . '/../.env');
+        define("SECRET_KEY", $_ENV['SECRET_KEY']);
+        define("BASE_URL", $_ENV["BASE_URL"]);
+        define("DB_HOST", $_ENV["DB_HOST"]);
+        define("DB_USER", $_ENV["DB_USER"]);
+        define("DB_PASSWORD", $_ENV["DB_PASSWORD"]);
+        define("DB_NAME", $_ENV["DB_NAME"]);
+        define("DB_PORT", $_ENV["DB_PORT"]);
+    } else {
+        define("SECRET_KEY", getenv("SECRET_KEY"));
+        define("BASE_URL", getenv("BASE_URL"));
+        define("DB_HOST", getenv("DB_HOST"));
+        define("DB_USER", getenv("DB_USER"));
+        define("DB_PASSWORD", getenv("DB_PASSWORD"));
+        define("DB_NAME", getenv("DB_NAME"));
+        define("DB_PORT", getenv("DB_PORT"));
+    }
 ?>
--- backend/public/index.php
+++ backend/public/index.php
@@ -7,9 +7,17 @@
     <title>EDEN Hotels - Backend</title>
 </head>
 
-<body style="display: flex; min-height: 100vh; justify-content: center; align-items: center;">
+<body style="display: flex; flex-direction: column; min-height: 100vh; justify-content: center; align-items: center;">
     <h1 style="text-align: center; text-transform: uppercase;">O Backend de EDEN Hotels está rodando com sucesso! 🫡
     </h1>
+    <br>
+    <?php
+        require __DIR__ . "/../vendor/autoload.php";
+        use App\Backend\Users;
+        $user = new Users();
+        $user_datas = $user->get_user("manassesndombele@freelancer.com", false, 1);
+        echo '<h2 style="text-align: center; text-transform: uppercase;">SUPER USUÁRIO: ' . $user_datas["username"] . ' </h2>';
+    ?>
 </body>
 
 </html>
