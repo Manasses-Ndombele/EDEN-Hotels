b3866f907177bd7d32541d2f2c9e9e85e0e1c31d
Manasses Ndombele <manassesndombelefreelancer@gmail.com>
2025-03-31 08:33:11 +0100

    configured the project for production

--- backend/config/index.php
+++ backend/config/index.php
@@ -2,24 +2,21 @@
     require __DIR__ . "/../vendor/autoload.php";
     use Symfony\Component\Dotenv\Dotenv;
 
-    define("DEBUG_MODE", true);
-    if (DEBUG_MODE) {
-        $dotenv = new Dotenv();
-        $dotenv->load(__DIR__ . '/../.env');
-        define("SECRET_KEY", $_ENV['SECRET_KEY']);
-        define("BASE_URL", $_ENV["BASE_URL"]);
-        define("DB_HOST", $_ENV["DB_HOST"]);
-        define("DB_USER", $_ENV["DB_USER"]);
-        define("DB_PASSWORD", $_ENV["DB_PASSWORD"]);
-        define("DB_NAME", $_ENV["DB_NAME"]);
-        define("DB_PORT", $_ENV["DB_PORT"]);
-    } else {
-        define("SECRET_KEY", getenv("SECRET_KEY"));
-        define("BASE_URL", getenv("BASE_URL"));
-        define("DB_HOST", getenv("DB_HOST"));
-        define("DB_USER", getenv("DB_USER"));
-        define("DB_PASSWORD", getenv("DB_PASSWORD"));
-        define("DB_NAME", getenv("DB_NAME"));
-        define("DB_PORT", getenv("DB_PORT"));
+    try {
+        define("DEBUG_MODE", $_ENV["DEBUG_MODE"]);
+        if (DEBUG_MODE) {
+            $dotenv = new Dotenv();
+            $dotenv->load(__DIR__ . '/../.env');
+        }
+    } catch (Exception $e) {
+        echo "Falha ao carregar as variáveis de ambiente!";
     }
+
+    define("SECRET_KEY", $_ENV['SECRET_KEY']);
+    define("BASE_URL", $_ENV["BASE_URL"]);
+    define("DB_HOST", $_ENV["DB_HOST"]);
+    define("DB_USER", $_ENV["DB_USER"]);
+    define("DB_PASSWORD", $_ENV["DB_PASSWORD"]);
+    define("DB_NAME", $_ENV["DB_NAME"]);
+    define("DB_PORT", $_ENV["DB_PORT"]);
 ?>
